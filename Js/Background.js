3ef2ff393a59d2624c0ca29817e1712acbd3fe55
Tewodros Birhanu <onesamket@gmail.com>
2023-06-05 00:42:10 +0300

    updating

--- Js/Background.js
+++ Js/Background.js
@@ -1,27 +1,27 @@
 
 
-// Get the navbar element
-var navbar = document.querySelector('header');
+// // Get the navbar element
+// var navbar = document.querySelector('header');
 
-// Set the initial scroll position to 0
-var scrollPosition = 0;
-// Add scroll event listener to the window
-window.addEventListener('scroll', function() {
-    // Calculate the new scroll position
-    var newScrollPosition = window.scrollY;
-    alert()
+// // Set the initial scroll position to 0
+// var scrollPosition = 0;
+// // Add scroll event listener to the window
+// window.addEventListener('scroll', function() {
+//     // Calculate the new scroll position
+//     var newScrollPosition = window.scrollY;
+//     alert()
 
-    // Check if scrolling up or down
-    if (scrollPosition < newScrollPosition) {
-        navbar.classList.add('.Header')
+//     // Check if scrolling up or down
+//     if (scrollPosition < newScrollPosition) {
+//         navbar.classList.add('.Header')
 
-        // Scrolling down, hide the navbar
-    } else {
-        // Scrolling up, show the navbar
-        navbar.classList.remove('.Header')
+//         // Scrolling down, hide the navbar
+//     } else {
+//         // Scrolling up, show the navbar
+//         navbar.classList.remove('.Header')
 
-    }
+//     }
 
-    // Update the scroll position
-    scrollPosition = newScrollPosition;
-});
+//     // Update the scroll position
+//     scrollPosition = newScrollPosition;
+// });
---
+++ Js/Projects.js
@@ -0,0 +1,248 @@
+const phpProject = [
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+];
+
+
+// for javascript pro 
+const JavascriptPro = [
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+];
+
+// for react Project 
+const React = [
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'get source code',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'get source code',
+
+    },
+   
+];
+
+const Backend = [
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+    {
+        name: 'Printer Managment system',
+        image: '../images/Profile/vue.png',
+        description: `In the CSS code, the footer is styled with a 
+        light background color and dark text color. The container within the footer is set to display as a flex container 
+        with space-between justification and center alignment.
+         This allows the location and contact information to be positioned side by side
+        `,
+        githubUrl: 'github.onesamket/Php',
+
+    },
+];
+
+
+
+
+var card = React.map(vaue =>
+    `<div class="card">
+    <div class="name">
+        <h2>${vaue.name}</h2>
+    </div>
+
+    <div class="image">
+        <img src="${vaue.image}" alt="">
+    </div>
+    <div class="desc">
+        <p>${vaue.description}</p>
+    </div>
+    <div class="github">
+        <a href="">${vaue.githubUrl}</a>
+    </div>
+</div>`)
+
+
+document.querySelector('.Projects').innerHTML=card;
+console.log(card)
